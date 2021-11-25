76099df2308d97653924bcbef8615781658cf263
mh-zeynal <mh.zeynal@gmail.com>
2021-11-25 14:00:35 +0330

    initializing project with cobra

---
+++ internal/commands/client.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package commands
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// clientCmd represents the client command
+var clientCmd = &cobra.Command{
+	Use:   "client",
+	Short: "client-side starter command",
+	Long: `this command starts a connection between client and server 
+and also lets user to send message to the server and receive answer`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("client called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(clientCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// clientCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// clientCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
